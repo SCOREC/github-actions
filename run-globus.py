76b30465270547422f6ebbe20a0fa83d20961fc3
Angelyr <scardking@gmail.com>
2024-11-07 15:26:48 -0800

    machine concurrency

--- run-globus.py
+++ run-globus.py
@@ -15,10 +15,6 @@ name = sys.argv[2]
 branch = sys.argv[3]
 endpoint = sys.argv[4]
 
-print("test")
-print(os.getcwd())
-print(os.listdir("."))
-
 with open(machine+'/env.sh', 'r') as file:
     env_file = file.read()
 
